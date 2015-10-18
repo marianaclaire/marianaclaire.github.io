352525e3662a9683036c10d8379040b0534d672b
ytixu <yi.t.xu@mail.mcgill.ca>
2015-10-18 18:43:34 -0400

    done :)

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -1,7 +1,8 @@
 var story = {
 	title: {
 		ENG: "My Story",
-		FREN: "Mon Histore"
+		FREN: "Mon histore",
+		ESP: "Mi historia"
 	},
 	content: {
 		ENG: [
@@ -20,12 +21,18 @@ var story = {
 		"Au changement énergétique du 2015, j'ai eu un éveil spiritual où j'ai été introduite au reiki et qui m'a inspiré à devenir végétarienne. C'est aussi à ce moment-là, que j'ai décidée de m'exposer au public et de partager mes habiletés avec les autres.",
 		"J'ai pour objectif d'aider les gens à ce souvenir de qui ils sont et de les aider a réveillé leurs pouvoirs personnels et leurs dons. Je crois que nous pouvons tous développer",
 		"nos habiletés et nos capacités oubliées. Je crois aussi que c'est une des raisons principales pour laquelle nous sommes sur terre en ce moment.",
+		],
+		ESP:[
+		"Nací con la habilidad de ver auras y espíritus y de poder comunicarme con ellos. Alrededor de los 5 años esta habilidad fue bloqueada después de que ingresara al sistema educativo, como ya nos pasa a muchos de nosotros. Crecí en la ciudad de Lima, Perú. Y desde muy pequeña me he expresado por medio de mis dibujos, mi voz, mis poesías, canciones y escritos. Siempre me ha encantado ayudar, aconsejar y sanar a las personas. Y no fue sino hasta años después en mi vida que me di cuenta de mis habilidades y las identifique. A los 18 años tome la decisión de desarrollar mis dones y de utilizarlos para ayudar a las personas. Recordé quien era en mi pasado y con qué propósito vine a esta vida. En realidad, esta es la primera vida en la que decidí tomar un camino más difícil y humilde que en mis vidas pasadas, en el sentido de que experimentaría lo que es nacer con dones, bloquearlos y esforzarme en desbloquearlos de nuevo paso a paso con el propósito de luego poder entender a las personas que dejaron de creer en sí mismos y oprimieron sus habilidades. De esta manera podría ayudarlos a volver a recordar sus habilidades y como desarrollarlas de nuevo. También decidí antes de nacer que en esta vida bloquearía mis dones al principio para poder experimentar lo que es ser un ser humano en todos sus aspectos.",
+		"En lo referente a mi educación, actualmente me encuentro estudiando un bachiller en psicología. Es algo que me ha permitido tener un mejor entendimiento de las personas a un nivel más profundo. Hasta ahora he logrado desbloquear mis dones de telepatía, sanación, clarividencia, clariestesia, clariaudiencia, postcognición y precognición. Actualmente me encuentro desarrollando más habilidades, pero estoy al tanto de que la paciencia y fluidez son unos de mis propósitos principales en esta vida y respeto mi decisión pre-natal de ir despacio y despertarlas una a una.",
+		"Durante el cambio de energía que ocurrió en el 2015 con la llegada de la luna roja, tuve otra experiencia con mi despertar espiritual que me inspiro a aprender Reiki y convertirme en vegetariana. Pero lo más importante de esta fase de mi vida es que tome la decisión de dar un paso hacia delante para compartir mis habilidades con otros públicamente. Mi objetivo es ayudar a las personas para que recuerden quienes son en realidad, y que despierten su poder personal. Yo considero que es algo que todos podemos hacer si nos comprometemos a hacer el trabajo necesario ya que es la razón principal del porque decidimos encarnar en esta vida.",
 		]
 	}
 }
 
 var serviceText = {
 	ENG: "I offer many different types of readings including: telepathic readings, oracle readings, soul drawings, reiki healing, sound healing and I also facilitate the aura alchemy process. I can help you determine what will be most beneficial. Most times it's a combination in the same session.",
-	FREN: "J'offre plusieurs types de services psychiques comme les lectures télépathiques, les lectures avec les cartes de tarot ou d'oracle, les dessins de l'âme, du reiki, la guérison par le son et le processus d'alchimie aurique. Je peux aussi vous aider à déterminer quel genre de service serait le plus convenable pour vous. La plupart du temps, pendant les sessions j'utilise une combinaison de la plupart de ses services."
+	FREN: "J'offre plusieurs types de services psychiques comme les lectures télépathiques, les lectures avec les cartes de tarot ou d'oracle, les dessins de l'âme, du reiki, la guérison par le son et le processus d'alchimie aurique. Je peux aussi vous aider à déterminer quel genre de service serait le plus convenable pour vous. La plupart du temps, pendant les sessions j'utilise une combinaison de la plupart de ses services.",
+	ESP: "Ofrezco diferentes tipos de lecturas psíquicas incluyendo las lecturas telepáticas, lecturas de tarot, oráculo, dibujos de alma, sanación Reiki, sanación del sonido, y procesos de curación usando integración y alquimia. También puedo ayudar a determinar qué servicio sería más beneficioso para cada persona, y por lo general en una sesión se hace una combinación de varios servicios."
 
 }
--- assets/scripts/services.js
+++ assets/scripts/services.js
@@ -2,96 +2,107 @@ var services = [
 	{
 		name: {
 			ENG:"Readings",
-			FREN:"Lectures Psychiques"
+			FREN:"Lectures Psychiques",
+			ESP:"Lecturas psíquicas"
+			},
+		slug: {
+			ENG:"readings",
+			FREN:"lectures",
+			ESP:"lecturas"
 			},
 		// description: "Readings are done via video call. I use my channeling abilities to channel your higher self, guides, angels and other humans. I can also go into your Akashic records if you would like to know about your past lives and I also like to pick up tarot and oracle cards.",
 		faIcon: "fa-eye",
 		faIconBase: "fa-sun-o",
-		slug: {
-			ENG:"readings",
-			FREN:"lectures"
-			},
 		color: "#FF6666"
 	},{
 		name: {
 			ENG:"Psychic Coaching",
-			FREN:"Entrainement Psychique"
+			FREN:"Entrainement Psychique",
+			ESP:"Entrenamiento psíquico",
 			},
+		slug: {
+			ENG:"coaching",
+			FREN:"entrainement",
+			ESP:"entrenamiento"
+			},
+		color: "#FF66CC",
 		// description: "During this session I will teach you techniques that will help you develop your psychic abilities. We will practice seeing auras, origins, using telepathy and channeling, reading cards, opening the third eye, etc.",
 		faIcon: "fa-graduation-cap ",
 		faIconBase: "fa-sun-o",
-		slug: "coaching",
-		color: "#FF66CC"
-	},{
-		name: {
-			ENG:"Soul Drawings",
-			FREN:"Dessins de l'âme"
-			},
-		// description: "Soul drawings contain messages from your higher self or guides. I will also include a report that will explain how to use the drawing to meditate and heal yourself and about how to interpret them. Its presence is healing to you because of its high frequency and colors.",
-		faIcon: "fa-pencil",
-		faIconBase: "fa-user",
-		slug: {
-			ENG:"drawings",
-			FREN:"dessins"
-			},
-		color: "#FF9933"
-	},{
-		name: {
-			ENG:"Soul Paintings",
-			FREN:"Peintures de l'âme"
-			},
-		// description: "Soul paintings differ from soul drawings because they are more detailed and because the painting takes more time to complete. Soul drawings are drawings of your aura field. I will draw your higher self, guides and the symbols, colors and messages that they want you to see. I will also include a report.",
-		faIcon: "fa-paint-brush ",
-		faIconBase: "fa-user",
-		slug: {
-			ENG:"paintings",
-			FREN:"peintures"
-			},
-		color: "#33CC33"//"#CCCC00"
 	},{
 		name: {
 			ENG:"Aura Alchemy Process",
-			FREN:"Processus d'alchimie aurique"
+			FREN:"Processus d'alchimie aurique",
+			ESP:"Proceso alquimista áurico"
 			},
+		slug: {
+			ENG:"alchemy",
+			FREN:"alchimie",
+			ESP:"alquimista"
+			},
+		color: "#FF9933",
 		// description: "I am a facilitator of Pamela Aaralyn's Aura Alchemy process. This is a process of integration that transmutes the negative imprints of a person into more positive and empowering feelings.",
 		faIcon: "fa-diamond",
 		faIconBase: "fa-sun-o",
-		slug: {
-			ENG:"alchemy",
-			FREN:"alchimie"
-			},
-		color: "#33CCCC"
 	},{
 		name: {
-			ENG:"Sound Healing",
-			FREN:"Sound Healing",
+			ENG:"Soul Drawings",
+			FREN:"Dessins de l'âme",
+			ESP:"Dibujos del alma"
 			},
-		// description: "I will sing and use chants during this session and I will combine this with color alchemy. \"Sound healing is the therapeutic application of sound frequencies to the body/mind of a person with the intention of bringing them into a state of harmony and health.\"",
-		faIcon: "fa-music",
-		faIconBase: "fa-heartbeat",
 		slug: {
-			ENG:"healing",
-			FREN:"healing",
+			ENG:"drawings",
+			FREN:"dessins",
+			ESP:"dibujos"
 			},
-		color: "#3366FF"
+		color: "#33CC33",//"#CCCC00"
+		// description: "Soul drawings contain messages from your higher self or guides. I will also include a report that will explain how to use the drawing to meditate and heal yourself and about how to interpret them. Its presence is healing to you because of its high frequency and colors.",
+		faIcon: "fa-pencil",
+		faIconBase: "fa-user",
 	},{
-	// 	name: "Chat Healing",
-	// 	description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus felis sit amet quam interdum, a cursus lectus ultrices. Quisque semper erat at sagittis eleifend. ",
-	// 	faIcon: "fa-comments-o",
-	// 	faIconBase: "fa-heart",
-	// 	color: "#3366FF"
-	// },{
+		name: {
+			ENG:"Soul Paintings",
+			FREN:"Peintures de l'âme",
+			ESP:"Pinturas del alma"
+			},
+		slug: {
+			ENG:"paintings",
+			FREN:"peintures",
+			ESP:"pinturas"
+			},
+		color: "#33CCCC",
+		// description: "Soul paintings differ from soul drawings because they are more detailed and because the painting takes more time to complete. Soul drawings are drawings of your aura field. I will draw your higher self, guides and the symbols, colors and messages that they want you to see. I will also include a report.",
+		faIcon: "fa-paint-brush ",
+		faIconBase: "fa-user",
+	},{
 		name: {
 			ENG:"Reiki",
-			FREN:"Reiki"
+			FREN:"Reiki",
+			ESP:"Reiki"
 			},
+		slug: {
+			ENG:"reiki",
+			FREN:"reiki-f",
+			ESP:"reiki-e"
+			},
+		color: "#3366FF",
 		// description: "I am currently a second degree Reiki practitioner. I offer traditional Usui Reiki sessions. I can do Reiki sessions in person if you live in Calgary or by distance using skype. My lineage is passed from Madame Hawayo Takata.",
 		faIcon: "fa-hand-paper-o",
 		faIconBase: "fa-sun-o",
-		slug: {
-			ENG:"reiki",
-			FREN:"reiki"
+	},{
+		name: {
+			ENG:"Sound Healing",
+			FREN:"Curaison avec le son",
+			ESP:"Sanación de sonido",
 			},
-		color: "#9966FF"
+		slug: {
+			ENG:"healing",
+			FREN:"curaison",
+			ESP:"sanación",
+			},
+		color: "#9966FF",
+		// description: "I will sing and use chants during this session and I will combine this with color alchemy. \"Sound healing is the therapeutic application of sound frequencies to the body/mind of a person with the intention of bringing them into a state of harmony and health.\"",
+		faIcon: "fa-music",
+		faIconBase: "fa-heartbeat",
 	}
 ];

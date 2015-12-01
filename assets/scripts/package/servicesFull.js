21eb48a4f82b5a10b15f4674bd3b02501a3ac109
yitian <yi.t.xu@mail.mcgill.ca>
2015-11-30 23:09:41 -0500

    update

--- assets/scripts/package/servicesFull.js
+++ assets/scripts/package/servicesFull.js
@@ -41,9 +41,17 @@ var servicesFull = {
 			},
 		color: "#33CC33",
 		description: {
-			ENG:["I offer two types of drawings. The first type is a drawing of your aura field. This drawing is very specific. I will draw your higher self at the middle surrounded by your guides, angels and ascended masters. I will illustrate your aura color and soul aspects as well as use symbols that symbolize your soul.",
-			"The second type of drawing is a more intuitive drawing, for this one I will use the symbols, messages and colors that would be the most healing to you in your current situation according to your guides and higher self. I can also include a report that will explain how to use the drawing to meditate and heal yourself and about how to interpret them. Its presence is healing to you because of its high frequency and colors.",
-			"This drawing is very specific. I will draw your higher self at the middle surrounded by your guides, source aspects and braids, animal guides, angels and ascended masters. I will illustrate your aura color, overlay and secondary colors. My specialty is empowering people. I do this by facilitating their connection through their source aspects, purposes, negative imprints and abilities. I love reading auras, origins, source aspects and life purposes because it is so empowering for people to know these! that is the reason why I do aura field drawings. These drawings show you your guides, who are your dominant source aspects and soul braids. It makes you remember who you are and also by having them close to you, you step into their vibration, your higher self vibration and this is very powerful. I will channel symbols from your most dominant source aspects that will help you step into their vibration. And I will explain all of this in the report. I will also write down the messages that your guides have for you. This is a really fun process and I love doing it for you. I do ship the drawing to your address. The drawing is 14 x 17 inches."],
+			ENG:["Soul drawings are empowering for people. They contain specific light frequencies that facilitate their connection to the vibration of their origin, source aspects, life purposes and abilities. Stepping into the vibration of your higher self or source aspects is very empowering and liberating because it helps people remember who they are and why they incarnated at this time. It is important to keep the drawing close to the place where you meditate and meditate with it. The report explains in detail what the drawing means. I ship the original copy of all the drawings. The shipping and report fee are included in all the prices. Take note that it will take around 2 or 3 weeks for you to receive the drawing after the day you made the payment.",
+			"<h3>1. Aura Field illustration ($95)</h3>This drawing illustrates the vibration of everything inside your aura field. I will draw your higher self in the middle, surrounded by your aura color, your spirit guides, animal guides, source aspects, ascended masters, archangels and channeled symbols. In order to do so I will use my clairvoyant and channeling abilities and access your Akashic records. The report will include: your aura color and what it means, the name of your higher self, each source aspect and spirit guide, followed by their own abilities, life purposes, negative imprints, messages, symbols and colors. The drawing is 14 x 17 inches. The report and shipping fee are included on the price.",
+			"<img width='100%' src='../assets/images/drawings/Sirian-Hyadian-Lemurian-and-Pleidian-source-aspects.jpg'>",
+			"<h3>2. Couple Aura Field illustration ($189)</h3>This drawing illustrates everything inside the relationship aura field. I will draw both higher selves in the middle, surrounded by the relationship aura color, spirit guides, animal guides, source aspects, ascended masters, archangels and channeled symbols. In order to do so I will use my clairvoyant and channeling abilities and access your Akashic records. This illustration can be done for any type of relationship between 2 people. The report will include: the relationship aura color and what it means, the name of their higher selves, each source aspect and spirit guide, followed by their own abilities, life purposes, negative imprints, messages, symbols and colors. The report also explains the type of relationship between these two people, their contracts together if they have some, past lives together and advice for the relationship. The drawing is 18 x 24 inches. The relationship report and shipping fee are included on the price.",
+			"<h3>3. Past Life Comic Book ($189)</h3>This is an illustration of one of your past lives. I will illustrate the past life that is the most significant to you at this moment or a specific past life at your request. In order to do so I will use my clairvoyant and channeling abilities and access your Akashic records. The past life is illustrated in the form of a comic book and a written short story. The comic book will be of 3 pages. Each page measures 10 x 15 inches. The short story and shipping fee are included on the price.",
+			"<h3>4. Soul Portrait ($65)</h3>This is an illustration of your higher self. Your higher self is the eternal, omnipotent, conscious aspect of your soul, who is one’s authentic/ real self and the highest version of you. In order to do so I will use my clairvoyant and channeling abilities. The report will include the name of your higher self, followed by his/her abilities, life purposes, negative imprints, messages, symbols and colors. The drawing is 10 x 15 inches. The report and shipping fee are included on the price.",
+			"<center><img src='../assets/images/drawings/Elsa-the-warrior-light-fairy.jpg'></center>",
+			"<h3>5. Spirit Guide illustration ($65)</h3>This is an illustration of one of your spirit guides. I will draw the spirit guide that is the most dominant at the time or a specific spirit guide at your request. In order to do so I will use my clairvoyant and channeling abilities. Note that your dominant spirit guides are your dominant sources aspects and braids. The report will include the name of your spirit guide followed by his/her abilities, life purposes, negative imprints, messages, symbols and colors. The drawing is 10 x 15 inches. The report and shipping fee are included on the price.",
+			"<h3>6. Intuitive drawing ($65)</h3>Intuitive drawings are always a surprise. For this drawing I will ask your higher self and guides to let me channel the symbols, messages, images and colors that would be the most healing to you in your current situation. It depends of the specific needs of a person at a time. Often this drawing is a grounding, third eye or DNA activation drawing. In order to do so I will use my clairvoyant and channeling abilities. The drawing is 10 x 15 inches. The report and shipping fee are included on the price.",
+			"<center><img src='../assets/images/drawings/1.jpg'></center>"
+			],
 			FREN:["Les dessins spirituels ont des fréquences de lumière spécifiques qui ont pour but de donner les moyens au gens pour se connecter avec la vibration de leur origine, aspects de source, objectifs de vie et leurs habilités. Donner un pas vers la vibration de ton âme et aspects de source est très libérateur parce que ça aide les gens à se rappeler qui ils sont et pourquoi ils se sont incarnés sur terre. C’est important de mettre le dessin près d’où vous faites vos méditations et de méditer avec lui. Le rapport explique en détail les images du dessin. Je vous envoi la copie originelle de votre dessin. Le prix de l’envoi et du rapport sont déjà inclus dans le prix total du dessin. Prenez note que ça prendra environ 2 à 3 semaines pour recevoir votre dessin après avoir fait le paiement.",
 				"<h3>1. Illustration du champ de l'Aura (95$)</h3>Ce dessin illustre la vibration de tout ce qui est dans le champ de ton aura.  Je vais dessiner votre âme au milieu entourée de ton aura, aspects de source, esprits guides, tes guides animaux, maîtres ascensionnés, archanges et symboles canalisés. Je vais utiliser mes habilités de canalisation et clairvoyance pour accéder à vos Archives Akashiques. Le rapport aura inclus la couleur de votre aura et qu’est-ce que cela signifie, le nom de votre âme, esprits guides et aspects de source, leurs objectifs de vie, empruntes négatives, habilités, messages, symboles et couleurs. Le dessin mesure 14 x 17 pouces. Le prix de l’envoi et du rapport sont déjà inclus dans le prix total du dessin.",
 				"<img width='100%' src='../assets/images/drawings/Sirian-Hyadian-Lemurian-and-Pleidian-source-aspects.jpg'>",
@@ -70,41 +78,41 @@ var servicesFull = {
 		faIconBase: "fa-user",
 		image:"g.jpg"
 	},
-	paintings: {
-		name: {
-			ENG:"Aura Field/Intuitive Painting",
-			FREN:"Peintures de l'aura/intuitive",
-			ESP:"Pinturas del aura/intuitiva"
-			},
-		slug: {
-			ENG:"paintings",
-			FREN:"peintures",
-			ESP:"pinturas"
-			},
-		color: "#33CCCC",
-		description: {
-			ENG: [
-				"Paintings differ from drawings because they are more detailed and because the painting takes more time to complete.",
-				"There are two types of paintings. The first type is a painting of your aura field. This painting is very specific. I will paint your higher self at the middle surrounded by your guides, angels and ascended masters. I will illustrate your aura color and soul aspects as well as use symbols that symbolize your soul.",
-				"The second type of aura field/intuitive painting is a more intuitive painting, for this one I will use the symbols, messages and colors that would be the most healing to you in your current situation according to your guides and higher self.",
-				"I can also include a report that will explain how to use the painting to meditate and heal yourself and about how to interpret them. Its presence is healing to you because of its high frequency and colors."
-				],
-			FREN: [
-				"Les peintures diffèrent des dessins, car elles sont beaucoup plus détaillés et sont plus longs à faire.",
-				"J’offre deux types de peintures. Le premier c’est une peinture de tout ce qui est dans votre aura. Cette peinture est très spécifique puisque je vais peinturer votre être supérieur au milieu entouré de vos guides, anges et les maîtres ascensionnés qui vous entourent. Je vais aussi illustrer la couleur de votre aura et les symboles qui vous décrivent le mieux.",
-				"Le deuxième type de peinture c’est une peinture intuitive où je vais peinturer des symboles, messages, esprits et des couleurs thérapeutiques spécifiques pour vous. Je peux aussi inclure un rapport qui donne l’interprétation de la peinture qui explique comment l’utiliser pour méditer et s’auto guérir. Les peintures de l'aura/intuitive sont une source de guérison à cause de la vibration élevée de leurs énergies."
-				],
-			ESP:[
-				"Las pinturas se diferencian de los dibujos por los materiales usados y el tiempo invertido en hacerlas.",
-				"Ofrezco dos tipos de pintura. El primer tipo es una pintura sobre todo lo que está dentro de su aura. Esta pintura es bastante específica. Pintare a su “yo” superior al medio de la pintura rodeado de sus guías, ángeles y maestros ascendidos. Ilustraré su origen y color de aura y los símbolos que más lo simbolicen.",
-				"El segundo tipo de pintura es uno más intuitivo y libre. En este usare símbolos, mensajes y colores que sean beneficiosos y terapéuticos para usted en la situación en la que se encuentra.",
-				"También puedo incluir un reporte que explique cómo usar el dibujo para meditar y sanarse a sí mismo. El reporte también explicaría como interpretar la pintura y sus mensajes. Su presencia es terapéutica para usted debido a la alta frecuencia que tiene y sus colores."
-				]
-			},
-		faIcon: "fa-paint-brush ",
-		faIconBase: "fa-user",
-		image:"i.jpg"
-	},
+	// paintings: {
+	// 	name: {
+	// 		ENG:"Aura Field/Intuitive Painting",
+	// 		FREN:"Peintures de l'aura/intuitive",
+	// 		ESP:"Pinturas del aura/intuitiva"
+	// 		},
+	// 	slug: {
+	// 		ENG:"paintings",
+	// 		FREN:"peintures",
+	// 		ESP:"pinturas"
+	// 		},
+	// 	color: "#33CCCC",
+	// 	description: {
+	// 		ENG: [
+	// 			"Paintings differ from drawings because they are more detailed and because the painting takes more time to complete.",
+	// 			"There are two types of paintings. The first type is a painting of your aura field. This painting is very specific. I will paint your higher self at the middle surrounded by your guides, angels and ascended masters. I will illustrate your aura color and soul aspects as well as use symbols that symbolize your soul.",
+	// 			"The second type of aura field/intuitive painting is a more intuitive painting, for this one I will use the symbols, messages and colors that would be the most healing to you in your current situation according to your guides and higher self.",
+	// 			"I can also include a report that will explain how to use the painting to meditate and heal yourself and about how to interpret them. Its presence is healing to you because of its high frequency and colors."
+	// 			],
+	// 		FREN: [
+	// 			"Les peintures diffèrent des dessins, car elles sont beaucoup plus détaillés et sont plus longs à faire.",
+	// 			"J’offre deux types de peintures. Le premier c’est une peinture de tout ce qui est dans votre aura. Cette peinture est très spécifique puisque je vais peinturer votre être supérieur au milieu entouré de vos guides, anges et les maîtres ascensionnés qui vous entourent. Je vais aussi illustrer la couleur de votre aura et les symboles qui vous décrivent le mieux.",
+	// 			"Le deuxième type de peinture c’est une peinture intuitive où je vais peinturer des symboles, messages, esprits et des couleurs thérapeutiques spécifiques pour vous. Je peux aussi inclure un rapport qui donne l’interprétation de la peinture qui explique comment l’utiliser pour méditer et s’auto guérir. Les peintures de l'aura/intuitive sont une source de guérison à cause de la vibration élevée de leurs énergies."
+	// 			],
+	// 		ESP:[
+	// 			"Las pinturas se diferencian de los dibujos por los materiales usados y el tiempo invertido en hacerlas.",
+	// 			"Ofrezco dos tipos de pintura. El primer tipo es una pintura sobre todo lo que está dentro de su aura. Esta pintura es bastante específica. Pintare a su “yo” superior al medio de la pintura rodeado de sus guías, ángeles y maestros ascendidos. Ilustraré su origen y color de aura y los símbolos que más lo simbolicen.",
+	// 			"El segundo tipo de pintura es uno más intuitivo y libre. En este usare símbolos, mensajes y colores que sean beneficiosos y terapéuticos para usted en la situación en la que se encuentra.",
+	// 			"También puedo incluir un reporte que explique cómo usar el dibujo para meditar y sanarse a sí mismo. El reporte también explicaría como interpretar la pintura y sus mensajes. Su presencia es terapéutica para usted debido a la alta frecuencia que tiene y sus colores."
+	// 			]
+	// 		},
+	// 	faIcon: "fa-paint-brush ",
+	// 	faIconBase: "fa-user",
+	// 	image:"i.jpg"
+	// },
 	alchemy: {
 		name: {
 			ENG:"Aura Alchemy Process",
@@ -174,15 +182,24 @@ var servicesFull = {
 		description: {
 			ENG:[
 				"I am currently a second degree Reiki practitioner. I offer traditional Usui Reiki sessions. I can do Reiki sessions in person if you live in Calgary or by distance using skype. My lineage is passed from Madame Hawayo Takata.",
-				"Reiki: \"healing technique based on the principle that the therapist can channel energy into the patient by means of touch, to activate the natural healing processes of the patient's body and restore physical and emotional well-being.\""
+				"Reiki: \"healing technique based on the principle that the therapist can channel energy into the patient by means of touch, to activate the natural healing processes of the patient's body and restore physical and emotional well-being.\"",
+				"<h2>My Certificates</h2>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki First Degree</a>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki Second Degree</a>"
 				],
 			FREN:[
 				"Je suis présentement praticienne de second degré reiki. J'offre des séances de reiki selon les enseignements Usui. Mes enseignements viennent directement de la lignée de Madame Hawayo Takata. Je peux offrir des guérisons reiki en personne si vous habitez à Calgary où à distance.",
 				"Le Reiki est une thérapie manuelle qui apporte de l’énergie à votre corps. Le praticien joue le rôle du vaisseau par lequel, en posant ses mains, il envoie l’énergie de la source au patient. L’énergie va naturellement là où le corps en a besoin. Le Reiki nous permet aussi de débloquer les blocages  ou tensions énergétiques de nos chakras et de nous centrer.",
+				"<h2>Mes certificats</h2>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki premier degré</a>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki second degré</a>"
 				],
 			ESP:[
 				"Actualmente tengo el segundo grado en la práctica de Reiki. Ofrezco sesiones tradicionales de Reiki Usui. Puedo hacer estas sesiones en persona si vive en Calgary, o a distancia por medio de llamadas audiovisuales. Mi linaje fue adquirido de Madame Hawayo Takata.",
 				"El Reiki es una terapia manual donde el practicante actúa como un canal de energía donde, poniendo las manos sobre el cuerpo del paciente, le transmite energía en las áreas de su cuerpo donde más lo necesita. El Reiki también nos permite desbloquear las tensiones en el cuerpo y los bloqueos en los chakras.",
+				"<h2>Mis certificados</h2>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki primer grado</a>",
+				"<a target='_blank' href='../assets/images/certificate/reiki1.jpg'>Reiki segundo grado</a>"
 				]
 			},
 		faIcon: "fa-hand-paper-o",

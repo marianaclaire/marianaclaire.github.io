4a5d14cb3590c3e220629cca4b9e02f6ff1d341b
yitian <yi.t.xu@mail.mcgill.ca>
2016-01-23 18:20:02 -0500

    temp change

---
+++ assets/scripts/package/prices.js
@@ -0,0 +1,193 @@
+var serviceText = {
+	ENG: "I offer many different types of readings, healings, activations, soul drawings and reports. Sessions are booked in time packages of 30 minutes, 45 minutes, 60 minutes, 75 minutes or 90 minutes. The prices are not per service but per time packages. I can help you determine what service will be most beneficial. Most times it's a combination of services and readings in the same session. ",
+	FREN: "J'offre plusieurs types de lectures, guérisons, activations, dessins spirituels et rapports canalisés. Mes sessions sont réservées par temps et non par service. Je peux aussi vous aider à déterminer quel genre de service serait le plus convenable pour vous par email. La plupart du temps, pendant les sessions j'utilise une combinaison de la plupart de ses services.",
+	ESP: "Ofrezco muchos tipos de lecturas, sanaciones, activaciones, dibujos espirituales y reportes canalizados. Las Sesiones son reservadas en paquetes de tiempo de 30 minutos, 45 minutos, 60 minutos, 75 minutos o 90 minutos. Los precios de paquetes son por tiempo no por servicio. Yo le puedo ayudar a determinar qué servicio sería el más adecuado y beneficioso para usted por email. La mayoría de veces es una combinación de servicios en la misma sesión.",
+
+	details: [
+		{
+			title: {
+				ENG: "Language",
+				FREN: "Langue",
+				ESP: "Lenguaje"
+			},
+			desc:{
+				ENG: "Available in English, French and Spanish",
+				FREN: "J'offre tous mes services en anglais, français et espagnol",
+				ESP: "Ofrezco todos mis servicios en inglés, español y francés"
+			},
+		},{
+			title: {
+				ENG: "Location",
+				FREN: "Lieu",
+				ESP: "Medios"
+			},
+			desc :{
+				ENG: "Skype, Google Hangouts, Facebook messenger or in person if you live in Calgary",
+				FREN: "Skype, Google Hangouts, Facebook messenger ou en personne si vous habitez à Calgary",
+				ESP: "Skype, Google Hangouts, Facebook messenger o en persona si vive en Calgary"
+			},
+		},{
+			title: {
+				ENG: "Shipping",
+				FREN: "Expédition",
+				ESP: "Envío"
+			},
+			desc :{
+				ENG: "If you have chosen any type of Soul Drawing, please email me the address where you would like me to ship the drawing with a picture of the person for whom the drawing is for. Take note that it will take around 2-3 weeks to receive the drawing after the day you made the payment.",
+				FREN: "Si vous voulez acheter un dessin spirituel S.V.P. me fournir l'adresse où je dois l'envoyer et aussi envoyez moi une photo de la personne pour laquelle le rapport est destiné. Prenez note que vous allez recevoir le dessin 2 à 3 semaines après avoir effectué le paiement.",
+				ESP: "Si ha elegido algún tipo de Dibujo Espiritual, por favor enviarme un mensaje por correo electrónico y adjunte la dirección a la que quiere que se le envié el dibujo junto a una foto de la persona para la cual se realizara el dibujo. Tener en cuenta que tomara cerca de 2 a 3 semanas en recibir el dibujo después de que se  haya hecho el pago."
+			}
+		}
+	],
+
+	payment: {
+		title: {
+			ENG: "Payment",
+			FREN: "Paiement",
+			ESP: "Pago"
+		},
+		paymentShortRedirect:{
+			ENG:"See full price list here.",
+			FREN:"Voici la liste des prix.",
+			ESP:"Vea la lista de precios aquí."
+		},
+		types:[
+			{
+				title: {
+					ENG: "Extrasensory session Payment",
+					FREN: "Paiement pour une Session",
+					ESP: "Tarifa para una Sesión"
+				},
+				price:{
+					ENG:[
+							"$50 for 30 mins",
+							"$75 for 45 mins",
+							"$100 for 60 mins",
+							"$125 for 75 mins",
+							"$150 for 90 mins"
+					],
+					FREN:[
+							"$50 pour 30 minutes",
+							"$75 pour 45 minutes",
+							"$100 pour 60 minutes",
+							"$125 pour 75 minutes",
+							"$150 pour 90 minutes"
+					],
+					ESP:[
+							"$50 por 30 minutos",
+							"$75 por 45 minutos",
+							"$100 por 60 minutos",
+							"$125 por 75 minutos",
+							"$150 por 90 minutos"
+					]
+				}
+			},{
+				title:{
+					ENG: "Channeled Report Payment",
+					FREN:"Paiement pour un Rapport canalisé",
+					ESP: "Tarifa para los Reportes Canalizados"
+				},
+				price:{
+					ENG: [
+							"$50 for a Cosmic Origin report",
+							"$50 for an Aura report",
+							"$50 for a Personal Channeling",
+							"$70 for a Relationship report",
+							"$70 for a Spirit Team report",
+							"$90 for an Personal Aura Field report",
+							"$150 for a Relationship Aura field report",
+							"$100 for a Past Life Short Story"
+					],
+					FREN:[
+							"$50 pour un Rapport d’Origine Cosmique",
+							"$50 pour un Rapport de l’Aura",
+							"$50 pour une Canalisation Personnalisée",
+							"$70 pour un Rapport de votre Relation",
+							"$70 pour un Rapport de votre Équipe Spirituelle",
+							"$90 pour un Rapport du Champs Aurique",
+							"$150 pour un Rapport du Champs Aurique de votre Relation",
+							"$100 pour une Histoire Courte de votre Vie Passée"
+					],
+					ESP:[
+							"$50 por un Reporte de Origen Cósmico",
+							"$50 por un Reporte Áurico",
+							"$50 por una Canalización Personalizada",
+							"$70 por un Reporte de la Relación",
+							"$70 por un Reporte de su Equipo Espiritual",
+							"$90 por un Reporte del Campo Áurico",
+							"$150 por un Reporte Áurico de su Relación",
+							"$100 por una Historia Corta sobre su vida pasada"
+					]
+				}
+			},{
+				title: {
+					ENG:"Shipping fee",
+					FREN:"Paiement pour l’Envoi",
+					ESP: "Tarifa de Envió"
+				},
+				price:{
+					ENG: [
+							"$10 for the shipping of a Higher Self illustration",
+							"$10 for the shipping of a Spirit Guide illustration",
+							"$10 for the shipping of an Intuitive drawing",
+							"$15 for the shipping of a Spirit Team illustration",
+							"$15 for the shipping of the Relationship illustration",
+							"$15 for the shipping of an Aura Field illustration",
+							"$20 for the shipping of a Couple Aura Field illustration",
+							"$10 for the shipping of a Past Life Comic Book"
+					],
+					FREN:[
+							"$10 pour l’envoi d’une Illustration de votre Moi Supérieur",
+							"$10 pour l’envoi d’une Illustration de votre Esprit Guide",
+							"$10 pour l’envoi d’un Dessin Intuitif",
+							"$15 pour l’envoi d’une Illustration de votre Relation",
+							"$15 pour l’envoi d’une Illustration de votre Équipe Spirituelle",
+							"$15 pour l’envoi d’une Illustration de votre Champs Aurique",
+							"$20 pour l’envoi d’une Illustration du Champs Aurique de votre Relation",
+							"$10 pour l’envoi d’une Bande-dessinée sur une vie passée"
+					],
+					ESP:[
+							"$10 por el envió de la Ilustración de su Yo Superior",
+							"$10 por el envió de la Ilustración de su Espíritu Guía",
+							"$10 por el envió de un Dibujo Intuitivo",
+							"$15 por el envió de la Ilustración de su Equipo Espiritual",
+							"$15 por el envió de la Ilustración de su Relación",
+							"$15 por el envió de la Ilustración de su Campo Áurico",
+							"$20 por el envió de la Ilustración del campo Áurico de su Relación.",
+							"$10 por el envió del Comic sobre su Vida pasada"
+					]
+				}
+			},{
+				title:{
+					ENG:"Package",
+					FREN:"Paiement pour un Dessin et un Rapport",
+					ESP:"Tarifa de Dibujos + Reportes"
+				},
+				price:{
+					ENG:[
+						"$155 for a Spirit Team illustration + Spirit Team report",
+						"$149 for a Relationship illustration + Relationship report",
+						"$289 for an Aura field illustration + Aura field report",
+						"$329 for a Couple Aura field illustration + Couple aura field report",
+						"$349 for a Past Life Comic Book + Past Life Short Story"
+					],
+					FREN:[
+						"$155 pour une Illustration et un rapport de votre Équipe Spirituelle",
+						"$149 pour une Illustration et un rapport de votre Relation",
+						"$289 pour une Illustration et un rapport de votre Champs Aurique",
+						"$329 pour une Illustration et un rapport du Champs Aurique de votre Relation",
+						"$349 pour une Bande-dessinée sur une vie passée + Histoire Courte de votre Vie Passée"
+					],
+					ESP:[
+						"$155 por una Ilustración de su Equipo Espiritual + Reporte de su Equipo Espiritual",
+						"$149 por una Ilustración de su Relación + Reporte de su Relación",
+						"$289 por una Ilustración de su Campo Áurico + Reporte del campo de Aura",
+						"$329 por una Ilustración del Campo Áurico de su Relación + Reporte del Campo Áurico de su Relación",
+						"$349 por un Comic sobre su Vida Pasada + Historia Corta sobre su Vida Pasada"
+					]
+				}
+			}
+		],
+		paypalDonate : "-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCTiJHG/BF6z2gp5yuDyGOzS612tZb+5JFlghiBMb1DEiQiZWhcrXwunHYuGMXcKzx/GVTvXXfHh+EP4RoQnLCF4oIe3thoaj6VV54Mg8eYFC+tpeAj7xeDRIYMDKRzO4bn6ASJqiNfhTNALk1uSclea5JFhOAgwobf86OwmNAH/zELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIxqY2XRcIKLeAgZgeiSXV6avVpyb9GarXgK+O3gvWasSCvjjKEDy199d/cRljpvF1/TYaZGQjGGwobNQ7NL8GCpCf8vv1kzwzdT0j5PwmM70y7iU+CaAu+9v3op8Qcq4l4QtaWp5cM6uQPUJ65VFlHL1BceX+cZR53ztAtOkNG48MEgHHtHEpmsxIQ25nscEm9nqaaP0lTHUvcsGhbOKcY8EXu6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MTAxOTAxMTQwOFowIwYJKoZIhvcNAQkEMRYEFOlNzsRSqEoByvOO05+dDDIY31ITMA0GCSqGSIb3DQEBAQUABIGAJaqgzNqo2t2aOR1QpF19mAOHLrOQytpiq5B15K60FW0NEYItQHLcnrwDO7aEhBdqlLIbf8GgCbeibB57Q5xpT50Lq2xiaT1FXw9vKzHaW26iD3bgWsEfp+h82Fksd7bE2Vcfj3Qb8UeYklAQt1TCsCjrFPVoINX2bGRJkWaQzX4=-----END PKCS7-----"
+	}
+}
